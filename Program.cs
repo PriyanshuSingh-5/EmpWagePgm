2db805724e82fee2e2c7c22d63aa72f690870b4f
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-07 23:58:57 +0530

    [Priyanshu]added switch case

--- Program.cs
+++ Program.cs
@@ -4,36 +4,39 @@ namespace EmpWage1
 {
     class Program
     {
+        //constants
+       public const int FULL_TIME = 1;
+      public const  int PART_TIME = 2;
+       public const int EMP_RATE_PER_HR = 20;
+        public const int EMP_TOTAL_WORKING_DAY_PER_MONTH = 20;
+
         static void Main(string[] args)
         {
-            //constants
-            int FULL_TIME = 1;
-            int PART_TIME = 2;
-            int EMP_RATE_PER_HR = 20;
-            int EMP_TOTAL_WORKING_DAY_PER_MONTH = 20;
-
+            
             //LOCAL VARIABLES
             int empHrs = 0;
             int empWage = 0;
 
             //inbuilt class
             Random random = new Random();
-            int employeeCheck = random.Next(0, 4);
+            int employeeCheck = random.Next(0, 5);
 
             Console.WriteLine("random value" + employeeCheck);
 
             //Selection statements
-            if(employeeCheck== FULL_TIME)
+           switch(employeeCheck)
             {
-                empHrs = 8;
-            }
-            else if(employeeCheck== PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case FULL_TIME:
+                    empHrs = 8;
+                    break;
+
+                case PART_TIME:
+                    empHrs = 4;
+                    break;
+
+                default:
+                    empHrs = 0;
+                    break;
             }
             //Computations
             empWage = EMP_RATE_PER_HR * empHrs * EMP_TOTAL_WORKING_DAY_PER_MONTH;
