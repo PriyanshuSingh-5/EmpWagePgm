a8884ee2322058d2236b2c2f1fdd80bac17f8d7b
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-07 23:42:34 +0530

    [priyanshu]added Wage per month

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace EmpWage1
             int FULL_TIME = 1;
             int PART_TIME = 2;
             int EMP_RATE_PER_HR = 20;
+            int EMP_TOTAL_WORKING_DAY_PER_MONTH = 20;
 
             //LOCAL VARIABLES
             int empHrs = 0;
@@ -17,7 +18,7 @@ namespace EmpWage1
 
             //inbuilt class
             Random random = new Random();
-            int employeeCheck = random.Next(0, 3);
+            int employeeCheck = random.Next(0, 4);
 
             Console.WriteLine("random value" + employeeCheck);
 
@@ -35,8 +36,8 @@ namespace EmpWage1
                 empHrs = 0;
             }
             //Computations
-            empWage = EMP_RATE_PER_HR * empHrs;
-            Console.WriteLine("Employee wage per day" + empWage);
+            empWage = EMP_RATE_PER_HR * empHrs * EMP_TOTAL_WORKING_DAY_PER_MONTH;
+            Console.WriteLine("Employee wage per MONTH" + empWage);
             Console.Read();
         }
     }
