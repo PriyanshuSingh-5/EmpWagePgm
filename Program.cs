a73f591b8a85a2107d0ad2740f9afb2fc2d2550b
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-07 22:56:13 +0530

    [Priyanshu]added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee wage problem");
+        }
+    }
+}
