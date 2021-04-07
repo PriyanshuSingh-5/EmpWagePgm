2c626afb788cfc5c0bb1430572a1c74eb4bb19b1
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-07 23:20:26 +0530

    [Priyanshu]added employee wage

--- Program.cs
+++ Program.cs
@@ -6,17 +6,32 @@ namespace EmpWage1
     {
         static void Main(string[] args)
         {
-            int present = 1;
+            //constants
+            int PRESENT = 1;
+            int EMP_RATE_PER_HR = 20;
 
+            //LOCAL VARIABLES
+            int empHrs = 0;
+            int empWage = 0;
+
+            //inbuilt class
             Random random = new Random();
             int employeeCheck = random.Next(0, 2);
 
             Console.WriteLine("random value" + employeeCheck);
-            if (employeeCheck == present)
-                Console.WriteLine("Employee is present");
-            else
-                Console.WriteLine("Employee is absent");
 
+            //Selection statements
+            if(employeeCheck== PRESENT)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            //Computations
+            empWage = EMP_RATE_PER_HR * empHrs;
+            Console.WriteLine("Employee wage per day" + empWage);
             Console.Read();
         }
     }
