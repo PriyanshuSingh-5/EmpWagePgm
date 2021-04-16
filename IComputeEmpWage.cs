5efb71687bd954c62c830fb3206e7f91f09aeda3
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-04-16 15:39:59 +0530

    [Priyanshu]modified daily wage and totalWage

---
+++ IComputeEmpWage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWage1
+{
+    class EmpWageComputation
+    {
+        public interface IComputeEmpWage
+        {
+            void AddCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+            void computeEmpWage();
+            int getTotalWage(string company);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,13 @@ namespace EmpWage1
 	{
 		static void Main(string[] args)
 		{
-			EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-			empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
-			empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+			Console.WriteLine("welcome to employee Program");
+			EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();  //creat object of EmpWageBuilderArray class
+			empWageBuilder.AddCompanyEmpWage("DMart", 20, 2, 10); //call AddCompany method
+			empWageBuilder.AddCompanyEmpWage("Reliance", 10, 4, 20);
 			empWageBuilder.computeEmpWage();
+
+			Console.Read();
 		}
 
 	}
